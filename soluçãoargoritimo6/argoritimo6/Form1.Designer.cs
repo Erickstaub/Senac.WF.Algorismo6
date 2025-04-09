628958d645c32145de07f2848b5106ff6364c955
Erick Staub <erickstaubfelipe@gmail.com>
2025-04-08 21:48:16 -0300

    aula de hoje

---
+++ soluçãoargoritimo6/argoritimo6/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace argoritimo6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            label9 = new Label();
+            groupBox2 = new GroupBox();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            textBox7 = new TextBox();
+            label5 = new Label();
+            textBox8 = new TextBox();
+            label6 = new Label();
+            label8 = new Label();
+            btnsair = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(192, 192, 255);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Location = new Point(50, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(658, 167);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(104, 130);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(538, 31);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(104, 93);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(538, 31);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(104, 56);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(538, 31);
+            textBox2.TabIndex = 8;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(104, 21);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(538, 31);
+            textBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 100);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(6, 139);
+            label9.Name = "label9";
+            label9.Size = new Size(59, 25);
+            label9.TabIndex = 6;
+            label9.Text = "label9";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(192, 255, 192);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Location = new Point(50, 228);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(658, 167);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(104, 130);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(538, 31);
+            textBox5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 131);
+            label7.Name = "label7";
+            label7.Size = new Size(59, 25);
+            label7.TabIndex = 8;
+            label7.Text = "label7";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(104, 93);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(538, 31);
+            textBox6.TabIndex = 13;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 97);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 25);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(104, 56);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(538, 31);
+            textBox7.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 61);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 25);
+            label5.TabIndex = 4;
+            label5.Text = "label5";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(104, 21);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(538, 31);
+            textBox8.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 27);
+            label6.Name = "label6";
+            label6.Size = new Size(59, 25);
+            label6.TabIndex = 3;
+            label6.Text = "label6";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(389, 249);
+            label8.Name = "label8";
+            label8.Size = new Size(59, 25);
+            label8.TabIndex = 7;
+            label8.Text = "label8";
+            // 
+            // btnsair
+            // 
+            btnsair.BackColor = Color.Red;
+            btnsair.FlatAppearance.BorderSize = 0;
+            btnsair.FlatStyle = FlatStyle.Popup;
+            btnsair.Font = new Font("Segoe UI", 12F);
+            btnsair.ForeColor = SystemColors.ButtonHighlight;
+            btnsair.Location = new Point(606, 428);
+            btnsair.Name = "btnsair";
+            btnsair.Size = new Size(112, 49);
+            btnsair.TabIndex = 8;
+            btnsair.Text = "Sair";
+            btnsair.UseVisualStyleBackColor = false;
+            btnsair.Click += btnsair_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(767, 499);
+            Controls.Add(btnsair);
+            Controls.Add(groupBox2);
+            Controls.Add(label8);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label9;
+        private Label label7;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button btnsair;
+    }
+}
---
+++ soluçãoargoritimo6/argoritimo6/Form1.cs
@@ -0,0 +1,30 @@
+namespace argoritimo6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
